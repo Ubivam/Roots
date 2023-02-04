0e7b7b505abd437fa80e87519c0031f666b53ca2
Aleksandar Paunovic <aleksandarpa@nordeus.com>
2023-02-04 13:22:40 +0100

    level cell

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Game/Level")]
 public class Level : ScriptableObject
 {
-	public List<Tile> Tiles;
+	public List<LevelCell> Tiles;
 	public int SideLength;
+}
+
+[Serializable]
+public class LevelCell
+{
+	public Tile Tile;
+	public float Rotation;
 }
